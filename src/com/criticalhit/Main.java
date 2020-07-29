6512034f3740b5bd6385ca63ab49ecb85d0eaf5a
Seattle Tupuhi <27191789+MinionSattle@users.noreply.github.com>
2020-07-30 11:18:17 +1200

    Init Commit

---
+++ src/com/criticalhit/Main.java
@@ -0,0 +1,10 @@
+package com.criticalhit;
+
+//Seattle Tupuhi 1286197
+//Jesse Whitten 1811972
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
