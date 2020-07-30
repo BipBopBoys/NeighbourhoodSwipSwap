f93632a0c1ce2229477eef0de06402ab8ce01c73
Seattle Tupuhi <27191789+MinionSattle@users.noreply.github.com>
2020-07-30 13:49:48 +1200

    Added comments of what to do
    Signed-off-by: Seattle Tupuhi <27191789+MinionSattle@users.noreply.github.com>

--- src/com/criticalhit/Main.java
+++ src/com/criticalhit/Main.java
@@ -1,12 +1,40 @@
 package com.criticalhit;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 //Seattle Tupuhi 1286197
 //Jesse Whitten 1811972
 public class Main {
 
+    Solution currentSolution;
+    Solution bestSolution;
+    List<Double> probList = new ArrayList<>();
+
     public static void main(String[] args) {
-	// write your code here
+	    //  Init boxes
+
+        // Init neighbourhoods
+
+        //while
+        // Choose destroy
+
+        //repair
+        //Acceptance
+        //replace best
+        //update prob
+
+        // end while
     }
+    //Sortbywidth
+
+    // repair bunnyhop
+
+    // destroy1
+
+    // destroy2
+
+    // destroy3
+
 }
---
+++ src/com/criticalhit/Solution.java
@@ -0,0 +1,8 @@
+package com.criticalhit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    List<Neighbourhood> rows = new ArrayList<>();
+}
