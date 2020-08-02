25ee4730539ee15fed84c244636a89711302415e
Seattle Tupuhi <27191789+MinionSattle@users.noreply.github.com>
2020-08-02 21:09:07 +1200

    Added csv files with boxes Implemented bufferedreader, stores boxes into init solution
    Signed-off-by: Seattle Tupuhi <27191789+MinionSattle@users.noreply.github.com>

--- src/com/criticalhit/Solution.java
+++ src/com/criticalhit/Solution.java
@@ -5,4 +5,32 @@ import java.util.List;
 
 public class Solution {
     List<Neighbourhood> rows = new ArrayList<>();
+    int width;
+    public Solution(int _width) {
+        width = _width;
+        createNewRow();
+    }
+    private void createNewRow(){
+        Neighbourhood neighbourhood = new Neighbourhood();
+        rows.add(neighbourhood);
+    }
+    public void initBox(Box newBox){
+        if(rows.get(rows.size()-1).getTotalWidth() + newBox.getWidth() > width){
+            createNewRow();
+        }
+        rows.get(rows.size()-1).setBox(newBox);
+    }
+    public void printSolution(){
+        for (Neighbourhood row: rows) {
+            row.printRow();
+        }
+        System.out.println("Total Height:"+totalHeight());
+    }
+    public int totalHeight(){
+        int height = 0;
+        for (Neighbourhood row: rows) {
+            height += row.getTotalHeight();
+        }
+        return height;
+    }
 }
